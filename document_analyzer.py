2b66208b32b78ccb993b8f3fd847ff0342c22197
Faith Deanon <afdeanon@gmail.com>
2022-03-10 23:26:46 -0800

    changed output

--- document_analyzer.py
+++ document_analyzer.py
@@ -13,12 +13,14 @@ for i in f1:
 
 sort_val = {val[0] : val[1] for val in sorted(d.items(), key = lambda x: (-x[1], x[0]))}
 
-sorted_dict=dict(sorted(sort_val.items(),key=operator.itemgetter(1),reverse=True))
+sorted_dict=dict(sorted(sort_val.items(),key=operator.itemgetter(1), reverse = True))
 
 c=0
+print()
 for i,j in sorted_dict.items():
     if c<5:
         print(i + ":",j)
     else:
         break
     c+=1
+
