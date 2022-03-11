7e29ecbd82b7740753fb3469534f697b28384237
Faith Deanon <afdeanon@gmail.com>
2022-03-10 21:34:30 -0800

    finished question 5

--- tracker.py
+++ tracker.py
@@ -1,2 +1,6 @@
-def func_counter):
+def count_calls(func):
+    def wrapper(*args, **kwargs):
+        wrapper.counter += 1
+    wrapper.counter = 0
+    return wrapperdef func_counter):
 	counter = 0
